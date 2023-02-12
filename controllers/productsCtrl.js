da19f1f5aa6eef25f7bff1eeeeb0eab21114bada
saqib <saqibraheem1999@gmail.com>
2023-02-12 21:32:58 +0500

    frontend added

--- controllers/productsCtrl.js
+++ controllers/productsCtrl.js
@@ -1,11 +1,59 @@
+const { query } = require('express');
 const Products = require('../models/productModel.js')
 
+/// filter sorting and paginating
+class APIfeatures {
+    constructor(query,queryString){
+        this.query = query;
+        this.queryString = queryString; 
+    }
+    filtering(){
+        const queryObj = {...this.queryString} //queryString = req.query
+        console.log({bafore:queryObj});
 
+        const excludedFields = ['page','sort','limit',]
+        excludedFields.forEach(el => delete(queryObj[el]))
+        
+        console.log({after:queryObj});
+
+        let queryStr = JSON.stringify(queryObj)
+        console.log({queryStr});
+        queryStr = queryStr.replace(/\b(gte|gt|lt|lte|regex)\b/g, match => '$' + match)
+
+        this.query.find(JSON.parse(queryStr))
+        return this;
+    }
+
+    sorting(){
+        if(this.queryString.sort){
+            const sortBy = this.queryString.sort.split(',').join(' ')
+            this.query = this.query.sort(sortBy)
+        }else{
+            this.query = this.query.sort('-createdAt')
+        }
+        return this;
+    }
+
+    paginating(){
+        const page = this.queryString.page * 1 || 1 
+        const limit = this.queryString.page * 1 || 3
+        const skip = (page - 1) * limit;
+        this.query = this.query.skip(skip).limit(limit)  
+        return this;
+    }
+}
+/////
 const productCtrl = {
    getProducts: async(req,res) =>{
     try {
-        const products = await Products.find()
-        res.json(products)
+        console.log("====>"+req.query);
+        const features = new APIfeatures(Products.find(),req.query).filtering().sorting().paginating()
+        const products = await features.query
+        res.json({
+            status:'success',
+            result: products.length,
+            products: products
+        })
     } catch (err) {
         return res.status(500).json({msg:err.message})
     }
